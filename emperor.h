4c9fc6bd51d95b1765fb9d59d786e7044f2756ca
Marco Pashkov <marcopashkov@gmail.com>
2014-10-29 23:24:48 -0700

    finished imperial march

--- emperor.h
+++ emperor.h
@@ -3,11 +3,24 @@ int emperor[] = {
     N_G3, N_G3, N_G3, N_DS3, N_AS3, N_G3, N_DS3, N_AS3, N_G3,
     N_D4, N_D4, N_D4, N_DS4, N_AS3, N_FS3, N_DS3, N_AS3, N_G3,
     N_G4, N_G3, N_G3, N_G4, N_FS4, N_F4, N_E4, N_DS4, N_E4, N_GS3, N_CS4,
+    N_C4, N_B3, N_AS3, N_A3, N_AS3, N_DS3, N_FS3,
+    N_DS3, N_FS3, N_AS3, N_G3, N_B3, N_DS4,
+
+    N_G4, N_G3, N_G3, N_G4, N_FS4, N_F4, N_E4, N_DS4, N_E4, N_GS3, N_CS4,
+    N_C4, N_B3, N_AS3, N_A3, N_AS3, N_DS3, N_FS3,
+
+    N_DS3, N_FS3, N_G3, N_DS3, N_AS3, N_G3,
 };
 
 int emperorDuration[] = {
     8, 8, 8, 16, 16, 8, 16, 16, 4,
     8, 8, 8, 16, 16, 8, 16, 16, 4,
-    8, 16, 16, 8, 16, 16, 32, 32, 16, 8, 4,
+    8, 16, 16, 8, 16, 16, 32, 32, 16, 16, 8,
+    16, 16, 32, 32, 16, 16, 8,
+    16, 16, 8, 16, 16, 4,
 
+    8, 16, 16, 8, 16, 16, 32, 32, 16, 16, 8,
+    16, 16, 32, 32, 16, 16, 8,
+
+    16, 16, 8, 16, 16, 4,
 };
